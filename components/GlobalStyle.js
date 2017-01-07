df2a3f33d45d9dcf98c0718d73f9dfc929c3dc09
Thiago Goveia <goveiat@gmail.com>
2017-01-07 21:40:25 -0200

    padding item drawer

--- components/GlobalStyle.js
+++ components/GlobalStyle.js
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     titulo: {textAlign:'center', fontWeight: 'bold', fontSize: 18},
     header: {backgroundColor: '#d54f16'},
     icon: {color: '#b82e00'},
-    lateralOverlay: {backgroundColor:'rgba(213,79,22, 1)',  padding: 5, elevation: 3},
+    lateralOverlay: {backgroundColor:'rgba(213,79,22, 1)',  padding: 5, elevation: 3, paddingLeft: 17},
     formRow: {minHeight: 60, borderBottomWidth: 1, borderColor: '#ebebeb', alignItems: 'center'},
     tituloCard: {marginLeft: 10, fontSize: 18},
     tituloCardContainer: { alignItems: 'center' }
