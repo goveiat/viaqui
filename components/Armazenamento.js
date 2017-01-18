93359e692959bb0137362bab53d65b11577a77ba
Thiago Goveia <goveiat@gmail.com>
2017-01-18 14:40:23 -0200

    Primeira função de sincronização

--- components/Armazenamento.js
+++ components/Armazenamento.js
@@ -1,5 +1,6 @@
 import {AsyncStorage} from 'react-native';
 import Storage from 'react-native-storage';
+import Utils from './Utils';
 
 
 //Definição do armazenamento local
@@ -8,5 +9,23 @@ global.storage = new Storage({
       storageBackend: AsyncStorage,
       defaultExpires: null,
       enableCache: false,
-      sync : {}
+      sync : {
+          //id: {pin}
+          aplicativo: function(params){
+              let { id, resolve, reject } = params;
+                Utils.post('https://www.viaqui.com.br/component/api/app/users/wsconfig/raw', id)
+                .then((retorno) => {
+                      storage.save({
+                          key: 'aplicativo',
+                          rawData: retorno,
+                      });
+                      resolve && resolve(retorno);
+                })
+                .catch((error) => {
+                    console.log(error);
+                    reject && reject(error);
+                });
+          }
+
+      }
 });
